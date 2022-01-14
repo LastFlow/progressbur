d5b757e3eb774779c9f7d9a3a2d9ee7b4bc85a75
LastFlow <somemoredevac@gmail.com>
2022-01-15 02:39:24 +0300

    added server.py

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect, session
 
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    print("kjoasdjasdjasl") 
-    app.run(host='127.0.0.1', port='48150', debug=True)
+    app.run(host='127.0.0.1', port='5000', debug=True)
+
+
+
